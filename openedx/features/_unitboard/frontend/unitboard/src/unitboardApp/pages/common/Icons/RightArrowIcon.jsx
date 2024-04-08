85b0b7bccbfc551ff5d6df91794fb67f5f5c017b
om <om.kumar@manprax.com>
2024-04-08 13:17:09 +0530

    added icon

---
+++ openedx/features/_unitboard/frontend/unitboard/src/unitboardApp/pages/common/Icons/RightArrowIcon.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RightArrowIcon = () => (
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 6 8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6Z" fill="currentColor"></path></svg>
+);
+
+export default RightArrowIcon;
