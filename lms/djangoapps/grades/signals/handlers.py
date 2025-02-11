ee1f15ca9a9a59717494648b3fe7a02fe44d3f44
DurgaSwetha <durga.garapati@tiss.edu>
2025-02-11 16:42:46 +0530

    Changes to remove the flag set to recaclulate grades

--- lms/djangoapps/grades/signals/handlers.py
+++ lms/djangoapps/grades/signals/handlers.py
@@ -257,7 +257,7 @@ def recalculate_course_grade_only(sender, course, course_structure, user, **kwar
     Updates a saved course grade, but does not update the subsection
     grades the user has in this course.
     """
-    CourseGradeFactory().update(user, course=course, course_structure=course_structure, force_update_subsections=True)
+    CourseGradeFactory().update(user, course=course, course_structure=course_structure)
 
 
 @receiver(ENROLLMENT_TRACK_UPDATED)
